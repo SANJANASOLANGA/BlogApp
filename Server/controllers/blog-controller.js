1fb268bf3d028cf648d6889c30af31a1f57e7c51
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2024-05-27 16:06:21 +0530

    Added addBlog api

--- Server/controllers/blog-controller.js
+++ Server/controllers/blog-controller.js
@@ -1,4 +1,6 @@
+const mongoose = require('mongoose');
 const BlogModel = require('../model/Blog.js');
+const User = require('../model/User.js');
 
 exports.getAllBlogs = async (req, res, next) => {
   let blogs;
@@ -12,5 +14,42 @@ exports.getAllBlogs = async (req, res, next) => {
     return res.status(404).json({ message: 'No Recipe Found' });
   }
   return res.status(200).json({ blogs });
+};
+
+exports.addBlog = async (req, res, next) => {
+  const { title, description, image, user } = req.body;
+  let existingUser;
+  try {
+    existingUser = await User.findById(user);
+    console.log('existingUser is ', existingUser)
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: err });
+  }
+  if (!existingUser) {
+    return res.status(400).json({ message: 'Unable to find user' });
+  }
+
+  const blog = new BlogModel({
+    title,
+    description,
+    image,
+    user,
+  });
+
+  try {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    await blog.save({ session });
+    existingUser.blogs.push(blog);
+    console.log('existingUser is ', existingUser)
+    await existingUser.save({ session });
+    await session.commitTransaction();
+  } catch (err) {
+    console.log('existingUser is ', existingUser)
+    console.log('error 1 is ', err);
+    return res.status(500).json({ message: err });
+  }
+  return res.status(200).json({ blog });
 };
 
