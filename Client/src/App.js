1c7d6dbce75249a34d52bd395db63f9de634c585
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2024-05-27 05:33:26 +0530

    Added routes

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
+import UserDetails from './components/auth/userDetails';
+import Blogs from './components/blog/Blogs';
+import CreateBlog from './components/blog/CreateBlog';
+import EditRecipes from './components/blog/EditBlog';
+import UpdateBlog from './components/blog/UpdateBlog';
+import AllBlogs from './components/blog/AllBlogs';
+import Footer from './components/Styles/Footer';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div>
+            <Routes>
+              <Route exact path="/" element={<Blogs />} />
+              <Route path="/sign-in" element={<SignIn />} />
+              <Route path="/sign-up" element={<SignUp />} />
+              <Route path="/userDetails" element={<UserDetails />} />
+              <Route path='/blogs' element={<Blogs />} />
+              <Route path='/create-blog' element={<CreateBlog />} />
+              <Route path='/edit-blogs' element={<EditRecipes />} />
+              <Route path="/update-blogs/:id" element={<UpdateBlog />}/>
+              <Route path="/recipes-admin" element={<AllBlogs />}/>              
+            </Routes>
+            <Footer/>
+        </div>
+      </Router>
   );
-}
+};
 
 export default App;
