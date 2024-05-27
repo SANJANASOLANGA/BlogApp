161c0a41186ccf60d5a50988319ea4f308c52f27
SANJANASOLANGA <sanjanasolangaarachchi@gmail.com>
2024-05-27 16:16:33 +0530

    Added register

--- Server/app.js
+++ Server/app.js
@@ -8,6 +8,10 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/blog', recipeRouter);
 
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "hvdvay6ert72839289()aiyg8t87qt72393293883uhefiuh78ttq3ifi78272jbkj?[]]pou89ywe";
+
 mongoose
   .connect('mongodb+srv://sanjanasolangaarachchi:WnQMY2bLReIw0LKU@recipe.pmdzzp3.mongodb.net/?retryWrites=true&w=majority')
   .then(() => {
@@ -20,3 +24,28 @@ mongoose
 
   require("./userDetails")
 
+const User = mongoose.model("UserInfo")
+app.post("/register", async (req, res) => {
+    const { fname, lname, email, password, userType } = req.body;
+    const encryptedPassword = await bcrypt.hash(password, 10);
+    try {
+      const oldUser = await User.findOne({ email });
+  
+      if (oldUser) {
+        return res.json({ error: "User Exists" });
+        // alert("User exists")
+      }
+      await User.create({
+        fname,
+        lname,
+        email,
+        password: encryptedPassword,
+        userType,
+      });
+      res.send({ status: "ok" });
+    } catch (error) {
+      res.send({ status: "error" });
+    }
+  });
+
+  
